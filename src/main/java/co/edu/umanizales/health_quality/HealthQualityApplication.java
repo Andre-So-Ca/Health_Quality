8c5d7a255bc63724984583e57d25f7f891bdd263
Andre-So-Ca <andrenpu@gmail.com>
2025-11-01 11:38:05 -0500

    Programación I - Sustentación I

---
+++ src/main/java/co/edu/umanizales/health_quality/HealthQualityApplication.java
@@ -0,0 +1,13 @@
+package co.edu.umanizales.health_quality;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthQualityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthQualityApplication.class, args);
+    }
+
+}
